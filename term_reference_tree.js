620ae47fbc9a9152dd6f6e4ca65f16da8acb8a03
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-05-23 17:13:14 +0100

    Working dynamic version.  Needs a lot of tidying and altering so that the code can be re-used.

--- term_reference_tree.js
+++ term_reference_tree.js
@@ -1,53 +1,29 @@
 (function($) {
-  $(function() {
-    // Bind the term expand/contract button to slide toggle the list underneath.
-    $('.term-reference-tree-button').click(function() {
-      $(this).toggleClass('term-reference-tree-collapsed');
-      $(this).siblings('ul').slideToggle('fast');
-    });
-
-    // An expand all button (unimplemented)
-    /*
-    $('.expandbutton').click(function() {
-      $(this).siblings('.term-reference-tree-button').trigger('click');
-    });
-    */
-
-
-    $('.term-reference-tree').each(function() {
-      // On page load, check whether the maximum number of choices is already selected.
-      // If so, disable the other options.
-      var tree = $(this);
-      checkMaxChoices(tree);
-      $(this).find('input[type=checkbox]').change(function() {
-        checkMaxChoices(tree);
-      });
-
-      // On page load, check if the start minimized option is selected.  If so,
-      // minimize each tree, except for lists that contain a checked box.   
-      if($(this).hasClass('term-reference-tree-start-minimized')) {
-        $(this).find('.term-reference-tree-button').each(function() {
-          // If no sibling <ul>'s contain a checked checkbox, add the
-          // term-reference-tree-collapsed class and hide the <ul>.
-          if($(this).siblings('ul').has('input[type=checkbox]:checked').size() == 0) {
-            $(this).addClass('term-reference-tree-collapsed');
-            $(this).siblings('ul').hide();
+  Drupal.behaviors.term_reference_tree = {
+    attach: function(context, settings){
+      $('.checkbox_tree_plus', context).click(function(){
+        if($(this).hasClass('plus')){
+          if($(this).siblings('ul').first().length){
+            $(this).siblings('ul').first().show();
+            $(this).removeClass('plus');
+            $(this).addClass('minus');
+          } else {
+            var original_click = $(this);
+            var tid = $(this).siblings('label').first().children().first().val();
+            var field_id = $(this).attr('id').split('__').shift();
+            $.getJSON(Drupal.settings.term_reference_tree.callback+'/'+tid+'/'+field_id, function(data){
+              $(original_click).parent().append(data[1]["data"]);
+              Drupal.attachBehaviors($(original_click).parent().children());
+              $(original_click).removeClass('plus');
+              $(original_click).addClass('minus');
+            });
           }
-        });
-      }
-    });
-  });
-
-  // This helper function checks if the maximum number of choices is already selected.
-  // If so, it disables all the other options.  If not, it enables them.
-  function checkMaxChoices(item) {
-    var maxChoices = item.attr('data-max-choices');
-    var count = item.find(':checked').length;
-    
-    if(maxChoices > 0 && count >= maxChoices) {
-      item.find('input[type=checkbox]:not(:checked)').attr('disabled', 'disabled').parent().addClass('disabled');
-    } else {
-      item.find('input[type=checkbox]').removeAttr('disabled').parent().removeClass('disabled');
+        } else {
+          $(this).siblings('ul').first().hide();    
+          $(this).removeClass('minus');
+          $(this).addClass('plus');    
+        }
+      });
     }
-  }
+  };
 })(jQuery);
