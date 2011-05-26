b60dcdca93957d182f90ab43cc25077c0fc99afb
Simon Rycroft <s.rycroft@nhm.ac.uk>
2011-05-26 13:29:30 +0100

    Now checks for max allowed values.

--- term_reference_tree.js
+++ term_reference_tree.js
@@ -1,60 +1,66 @@
-(function($) {
+(function($){
   Drupal.behaviors.term_reference_tree = {
     attach: function(context, settings){
-      $('.options_tree_plus', context).click(function(){
-        if($(this).hasClass('plus')){
-          if($(this).siblings('ul').first().length){
-            $(this).siblings('ul').first().slideDown();
-            $(this).removeClass('plus').addClass('minus');
-          } else {
-            var original_click = $(this);
-            $.getJSON(Drupal.settings.term_reference_tree.callback+'/'+$(this).siblings('label').first().children('input').first().val()+'/'+$(this).attr('id').split('__').shift(), function(data){
-              $(original_click).parent().append(data[1]["data"]);
-              Drupal.attachBehaviors($(original_click).parent().children());
-              $(original_click).removeClass('plus').addClass('minus');
-            });
-          }
-        } else {
-          $(this).siblings('ul').first().slideUp();
-          $(this).removeClass('minus').addClass('plus');
-        }
+      $('.options_tree_plus', context)
+          .click(
+              function(){
+                // Click the plus/minus buttons
+                if($(this).hasClass('plus')) {
+                  if($(this).siblings('ul').first().length) {
+                    $(this).siblings('ul').first().slideDown();
+                    $(this).removeClass('plus').addClass('minus');
+                  } else {
+                    var original_click = $(this);
+                    var name = $(this).attr('id').split('__').shift();
+                    if(name.indexOf('[')) {
+                      var name_no_array = name.split('[').shift();
+                    } else {
+                      name_no_array = name;
+                    }
+                    $
+                        .getJSON(
+                            Drupal['settings']['term_reference_tree'][name_no_array]['callback']
+                                + '/'
+                                + $(this).siblings('label').first().children(
+                                    'input').first().val() + '/' + name,
+                            function(data){
+                              $(original_click).parent()
+                                  .append(data[1]["data"]);
+                              Drupal.attachBehaviors($(original_click).parent()
+                                  .children());
+                              $(original_click).removeClass('plus').addClass(
+                                  'minus');
+                            });
+                  }
+                } else {
+                  $(this).siblings('ul').first().slideUp();
+                  $(this).removeClass('minus').addClass('plus');
+                }
+              });
+      $('input[type="checkbox"]', context).each(function(){
+        checkMaxChoices($(this));
+      });
+      $('input[type="checkbox"]', context).click(function(){
+        checkMaxChoices($(this));
       });
     }
   };
-})(jQuery);
-
-/*(function($) {
-  $(function() {
-    $('.term-reference-tree-button').click(function() {
-      $(this).toggleClass('term-reference-tree-collapsed');
-      $(this).siblings('ul').slideToggle('fast');
-    });
-    
-    $('.expandbutton').click(function() {
-      $(this).siblings('.term-reference-tree-button').trigger('click');
-    });
-     
-    $('.term-reference-tree').each(function() {
-      var tree = $(this);
-      checkMaxChoices(tree);
-      $(this).find('input[type=checkbox]').change(function() {
-        checkMaxChoices(tree);
-      });
-      
-      if($(this).hasClass('term-reference-tree-start-minimized')) {
-        $(this).find('.term-reference-tree-button').addClass('term-reference-tree-collapsed').siblings('ul').hide();
-      }
-    });
-  });
-  
-  function checkMaxChoices(item) {
-    var maxChoices = item.attr('data-max-choices');
-    var count = item.find(':checked').length;
-    
-    if(maxChoices > 0 && count >= maxChoices) {
-      item.find('input[type=checkbox]:not(:checked)').attr('disabled', 'disabled').parent().addClass('disabled');
+  function checkMaxChoices(item){
+    name = item.attr('name');
+    if(name.indexOf('[')) {
+      var name_no_array = name.split('[').shift();
     } else {
-      item.find('input[type=checkbox]').removeAttr('disabled').parent().removeClass('disabled');
+      name_no_array = name;
+    }
+    if(Drupal['settings']['term_reference_tree'][name_no_array]) {
+      var checked_count = $('input[name="' + item.attr('name') + '"]:checked').length;
+      if(checked_count >= Drupal['settings']['term_reference_tree'][name_no_array]['max']) {
+        $('input[name="' + item.attr('name') + '"]:not(:checked)').attr(
+            'disabled', 'disabled');
+      } else {
+        $('input[name="' + item.attr('name') + '"]:not(:checked)').removeAttr(
+            'disabled');
+      }
     }
   }
-})(jQuery);*/
+})(jQuery);
